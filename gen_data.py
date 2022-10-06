a8863c86a0a6ff0929203fb9b9c3cc389ae80dec
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2022-10-06 23:50:22 +0200

    ...

--- gen_data.py
+++ gen_data.py
@@ -5,12 +5,46 @@ from random import randint, choice, sample
 from datetime import datetime, timedelta
 from django.utils import timezone
 
-rates = {
+RATES = {
     "Technician": 600,
     "Chief": 800,
     "Lighting Designer": 900,
     "Multimedia Designer": 900,
 }
+
+DATES = [
+    1610085128,
+    1610699703,
+    1611674166,
+    1612355511,
+    1612530260,
+    1612841906,
+    1614148053,
+    1614757976,
+    1616696705,
+    1617383874,
+    1620269318,
+    1621163232,
+    1621363682,
+    1622776740,
+    1623024608,
+    1624120087,
+    1624201270,
+    1624897464,
+    1626067482,
+    1629942162,
+    1630294981,
+    1632273003,
+    1633957196,
+    1634359368,
+    1634925493,
+    1636425557,
+    1636804272,
+    1637648542,
+    1637711002,
+    1638690208,
+]
+
 
 def add_users():
     # coordinator
@@ -53,7 +87,7 @@ def add_users():
         for func in functions:
             usr.functions.add(func)
             rate = FunctionRate.objects.get(worker=usr, function=func)
-            rate.value = rates.get(func.name, 600)
+            rate.value = RATES.get(func.name, 600)
             rate.overtime = round(rate.value * 0.1)
             rate.save()
 
@@ -62,7 +96,7 @@ def add_events():
     coordinator = User.objects.filter(is_coordinator=True).first()
     account_manager = User.objects.filter(is_account_manager=True).first()
 
-    for i in range(1, 100):
+    for i in range(1, 30):
         e = Event()
         e.name = f"Event{i}"
         e.number = f"21-{i:02}"
@@ -77,16 +111,19 @@ def gen_workdays():
             is_coordinator=False, is_account_manager=False, is_superuser=False
         ).all()
     )
-    r = requests.get("https://api.lrs.org/random-date-generator?year=2021")
-    dates = r.json()["data"]
+    # r = requests.get("https://api.lrs.org/random-date-generator?year=2021")
+    # dates = r.json()["data"]
     events_list = Event.objects.all()
     func = Function.objects.all()
     days_batch = []
     for event in events_list:
         # print("Event: ", event)
-        for date in sample(list(dates.values()), k=randint(0, len(dates))):
+        # TODO for events need individials date for workers
+        dates_list = sample(DATES, k=randint(0, len(DATES)))
+        print(dates_list)
+        for date in DATES:
             # print("  Date: ", str(datetime.fromtimestamp(date["unix"])))
-            days_batch.extend(get_days(date["unix"], event, func, workers))
+            days_batch.extend(get_days(date, event, func, workers))
     print("Batch Sizie", len(days_batch))
     EventDayWork.objects.bulk_create(days_batch)
 
@@ -96,7 +133,8 @@ def get_days(date, event, func, workers):
     day_start = datetime.fromtimestamp(date)
     day_end = day_start + timedelta(hours=randint(8, 24))
     batch = []
-    for worker in sample(workers, k=gen_for_no_workers):
+    workers_list = sample(workers, k=gen_for_no_workers)
+    for worker in workers_list:
         day = EventDayWork()
         day.event = event
         day.start = timezone.make_aware(day_start)
